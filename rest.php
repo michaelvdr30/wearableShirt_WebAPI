136ba072a4967314d9b7c9b512dc4e2898302c9d
Michael van der Raad <michael.van.der.raad@hva.nl>
2018-11-29 12:33:35 +0100

    Provide database connection and wait for post requests

--- rest.php
+++ rest.php
@@ -1,3 +1,15 @@
 <?php
-    
+    include_once('database/db_connect.php');
+
+    $query = $connection->prepare("INSERT INTO test_data (data) VALUES (?)");
+
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if(isset($_POST['sensor_data'])) {
+            $val = mysqli_real_escape_string($connection, $_POST['sensor_data']);
+            $query->bind_param("s", $val);
+            $query->execute();
+        }
+        echo "success";
+    }
 ?>
